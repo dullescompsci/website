b1cf8ae94a6aec18eb3bf57b65c05c766b0f28ce
Vikachubro21 <57052932+Vikachubro21@users.noreply.github.com>
2020-08-08 17:31:27 -0500

    Update about.php

--- about.php
+++ about.php
@@ -25,7 +25,9 @@
 	<!-- Main Page -->
 	<div class="container" style="height:100%;">
 		<h1 class="display-3">About Us</h1>
+		<hr>Work in Progress, We are working on it!</hr>
 		<hr class="style14">
+		
 	</div>
 
 	<!-- Footer -->
