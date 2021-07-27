9c93d5e4f27e7e83f7db805f395ecdeb7e0b443b
Vikachubro21 <57052932+Vikachubro21@users.noreply.github.com>
2021-07-26 21:58:33 -0500

    I'm still bad, but better

--- html2.0/my-app/src/components/Header.js
+++ html2.0/my-app/src/components/Header.js
@@ -1,46 +1,36 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './Header.css';
 
 function Header(props) {
-  return (
-    <div className="navigation">
-      <nav class="navbar">
-        <div class="row">
-          <div class="column">
-            <Link class="navbar-logo" to="/">
-                <img src="dullescs.png" alt="Dulles Logo, ofc" width="200" height="75"></img>
-            </Link>
-          </div>
-
-          <div class="column">
-            <ul class="">
-                <Link class="nav-link" to="/">
-                  Home
-                  <span class="sr-only">(current)</span>
-                </Link>
-
-                <Link class="nav-link" to="/news">
-                  Announcements
-                </Link>
-
-                <Link class="nav-link" to="/resources">
-                  Member Resources
-                </Link>
-
-                <Link class="nav-link" to="/archive">
-                  Archive
-                </Link>
-
-                <Link class="nav-link" to="/about">
-                   About Us
-                </Link>
-            </ul>
-          </div>
+    return (
+        <div className="navigation">
+            <nav class="navbar">
+                <div class="logo headertext">
+                    <Link class="left" to="/">
+                        <img src="Capture.svg" alt="The logo, duh"></img>
+                    </Link>
+                </div>
+                <div class="navmenu">
+                    <Link class="headertext" to="/">
+                        Home
+                    </Link>
+                    <Link class="headertext" to="/news">
+                        News
+                    </Link>
+                    <Link class="headertext" to="/resources">
+                        Resources
+                    </Link>
+                    <Link class="headertext" to="/archive">
+                        Archive
+                    </Link>
+                    <Link class="headertext" to="/about">
+                            About Us
+                    </Link>
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+    );
 }
 
 export default Header;
