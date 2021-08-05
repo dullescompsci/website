1e02a9ca8d61473afe84dd67babbf4040b023c82
Vikachubro21 <57052932+Vikachubro21@users.noreply.github.com>
2021-08-05 10:49:40 -0500

    Updated Officers.js

--- html2.0/my-app/src/components/Officers.js
+++ html2.0/my-app/src/components/Officers.js
@@ -1,28 +1,96 @@
 //VIKAS add officerimg at the top underneath title
 
 import React from "react";
+import './Officers.css';
 
 function Officers() {
   return (
     <div className="officers">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Officers</h1>
-            <p>
-              Contact Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
+            </div>
+                  <div class="row">
+                      <img src="dullescs.png" class="officerImgLeft"/>
+                      <p class="officerTextRight">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                  </div>
+                  <div class="row">
+                      <img src="dullescs.png" class="officerImgRight" />
+                      <p class="officerTextLeft">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                  </div>
+                  <div class="row">
+                      <img src="dullescs.png" class="officerImgLeft" />
+                      <p class="officerTextRight">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                  </div>
+                  <div class="row">
+                      <img src="dullescs.png" class="officerImgRight" />
+                      <p class="officerTextLeft">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                  </div><div class="row">
+                      <img src="dullescs.png" class="officerImgLeft" />
+                      <p class="officerTextRight">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                  </div>
+                  <div class="row">
+                      <img src="dullescs.png" class="officerImgRight" />
+                      <p class="officerTextLeft">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                  </div>
+                  <div class="row">
+                      <img src="dullescs.png" class="officerImgLeftX" />
+                      <img src="dullescs.png" class="officerImgCenterX" />
+                      <img src="dullescs.png" class="officerImgRightX" />
+                     
+                  </div>
+                  <div class="row">
+                      <p class="officerTextLeftX">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p><p class="officerTextRightX">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                      <p class="officerTextCenterX">
+                          Contact Ipsum is simply dummy text of the printing and typesetting
+                          industry. Lorem Ipsum has been the industry's standard dummy text
+                          ever since the 1500s, when an unknown printer took a galley of
+                          type and scrambled it to make a type specimen book.
+                </p>
+                      
+
+                  </div>
         </div>
       </div>
     </div>
