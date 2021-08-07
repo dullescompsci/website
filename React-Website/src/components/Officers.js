730835d4052e8a39ea98bedc2047fe3aaaecb341
Vikachubro21 <57052932+Vikachubro21@users.noreply.github.com>
2021-08-06 22:26:46 -0500

    Updated Quotes

--- React-Website/src/components/Officers.js
+++ React-Website/src/components/Officers.js
@@ -23,48 +23,43 @@ function Officers() {
                   <div class="row">
                       <img src="president_1.png" class="officerImgLeft"/>
                       <p class="officerTextRight">
-                          Contact Ipsum is simply dummy text of the printing and typesetting
-                          industry. Lorem Ipsum has been the industry's standard dummy text
-                          ever since the 1500s, when an unknown printer took a galley of
-                          type and scrambled it to make a type specimen book.
+                          They called me a madman. And then they called me Viking the Totem Pole. <br />
+                          At least I'm the mascot, I thought, but then they replaced me with a statue out front<br />
+                          - Vikas Thoutam, Co-president
                 </p>
                   </div>
                   <div class="row">
                       <img src="president_2.png" class="officerImgRight" />
                       <p class="officerTextLeft">
-                          Contact Ipsum is simply dummy text of the printing and typesetting
-                          industry. Lorem Ipsum has been the industry's standard dummy text
-                          ever since the 1500s, when an unknown printer took a galley of
-                          type and scrambled it to make a type specimen book.
+                          Guys, I think Farmer John set a trap for m... YO! A PIECE OF HAY! <br />
+                          - Sahil Chowdhury, Co-president
                 </p>
                   </div>
                   <div class="row">
                       <img src="vice_President_1.png" class="officerImgLeft" />
                       <p class="officerTextRight">
-                          My name is Amogha Pokkulandra, affectionately known as "Big Amoghers". I like coding and mobile games because I am broke and have no PC. Do refrain from making any amogus references in my vicinity. POGCHAMP!!!
+                          My name is Amogha Pokkulandra, affectionately known as "Big Amoghers". I like coding and mobile games because I am broke and have no PC. Do refrain from making any amogus references in my vicinity. POGCHAMP!!! <br />
+                          - Amogha Pokkuandra, Vice President
                 </p>
                   </div>
                   <div class="row">
                       <img src="vice_President_2.png" class="officerImgRight" />
                       <p class="officerTextLeft">
-                          My last name is misspelled.
+                          My last name is misspelled. <br />
+                          - Dylan You, Vice President
                 </p>
                   </div><div class="row">
                       <img src="secretary.png" class="officerImgLeft" />
                       <p class="officerTextRight">
-                          Contact Ipsum is simply dummy text of the printing and typesetting
-                          industry. Lorem Ipsum has been the industry's standard dummy text
-                          ever since the 1500s, when an unknown printer took a galley of
-                          type and scrambled it to make a type specimen book.
+                          Quote didn't make it in time :( <br />
+                          - Suhani Goswami, Secretary
                 </p>
                   </div>
                   <div class="row">
                       <img src="historian.png" class="officerImgRight" />
                       <p class="officerTextLeft">
-                          Contact Ipsum is simply dummy text of the printing and typesetting
-                          industry. Lorem Ipsum has been the industry's standard dummy text
-                          ever since the 1500s, when an unknown printer took a galley of
-                          type and scrambled it to make a type specimen book.
+                          Quote didn't make it in time :( <br />
+                          - Rishindra Davuluri, Historian
                 </p>
                   </div>
                   <div class="row">
@@ -75,15 +70,15 @@ function Officers() {
                   </div>
                   <div class="row">
                       <p class="officerTextLeftX">
-                          Contact Ipsum is simply dummy text of the printing and typesetting
-                          industry. Lorem Ipsum has been the industry's standard dummy text
-                          ever since the 1500s, when an unknown printer took a galley of
-                          type and scrambled it to make a type specimen book.
+                          Quote didn't make it in time :( <br />
+                          - Soham (Insert Last Name Here, I'm sorry), Novice Coach
                 </p><p class="officerTextRightX">
-                          A Big Mango Potato.<br/>No seriously, he asked for this.
+                          A Big Mango Potato.<br />No seriously, he asked for this.<br />
+                          - Rushil Shah, Advanced Coach
                 </p>
                       <p class="officerTextCenterX">
-                          Insert Here.
+                          Quote didn't make it in time :( <br />
+                          - Athulya Saravanakumar, Novice Coach
                 </p>
                       
 
